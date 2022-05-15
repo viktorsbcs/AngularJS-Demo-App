2721acda4bcdbef0e5f32825fe6a9092f9462a60
viktorsbcs <electricjackiebeats@gmail.com>
2022-05-15 18:49:11 +0300

    Adding nav tab filtering for users

--- app.js
+++ app.js
@@ -36,77 +36,88 @@ var userApp = angular
 			$scope.user = {};
 			$scope.myForm.name.$touched = false;
 			$scope.myForm.email.$touched = false;
+		}
 
 
 
+
+		$scope.filtText = '';
+		$scope.select = function (setTab) {
+			$scope.filtText = "All";
+			if (setTab === 2) $scope.filtText = "Administrator";
+			if (setTab === 3) $scope.filtText = "Moderator";
+			if (setTab === 4) $scope.filtText = "Subscriber";
+
+			console.log("select() pressed, setTab=" + $scope.tab + ", fillText=" + $scope.filtText)
 
 		}
 
-		$scope.populateWithDummyData = function() {
+
+		$scope.populateWithDummyData = function () {
 			$scope.userList = [];
 			$scope.userCounter = 0
 
-			$scope.user = { name: "Sam Anthony", email: "sam@gmail.com", role: "Administrator", userId: 0 };		
+			$scope.user = { name: "Sam Anthony", email: "sam@gmail.com", role: "Administrator", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "Bridget Jones", email: "bridget@inbox.com", role: "Moderator", userId: 0 };		
+			$scope.user = { name: "Bridget Jones", email: "bridget@inbox.com", role: "Moderator", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "Karl Thomson", email: "karl@yahoo.com", role: "Moderator", userId: 0 };		
+			$scope.user = { name: "Karl Thomson", email: "karl@yahoo.com", role: "Moderator", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "Justine Adamson", email: "justine@gmail.com", role: "Subscriber", userId: 0 };		
+			$scope.user = { name: "Justine Adamson", email: "justine@gmail.com", role: "Subscriber", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "Aleksandr Nowitzki", email: "aleksandr@gmail.com", role: "Subscriber", userId: 0 };		
+			$scope.user = { name: "Aleksandr Nowitzki", email: "aleksandr@gmail.com", role: "Subscriber", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "William Manor", email: "william@gmail.com", role: "Administrator", userId: 0 };		
+			$scope.user = { name: "William Manor", email: "william@gmail.com", role: "Administrator", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "George Bean", email: "george@gmail.com", role: "Moderator", userId: 0 };		
+			$scope.user = { name: "George Bean", email: "george@gmail.com", role: "Moderator", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "Jack Depp", email: "jack@outlook.com", role: "Subscriber", userId: 0 };		
+			$scope.user = { name: "Jack Depp", email: "jack@outlook.com", role: "Subscriber", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "Branda Simpson", email: "branda@abc.com", role: "Subscriber", userId: 0 };		
+			$scope.user = { name: "Branda Simpson", email: "branda@abc.com", role: "Subscriber", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			$scope.user = { name: "Bill Russel", email: "bill@info.com", role: "Subscriber", userId: 0 };		
+			$scope.user = { name: "Bill Russel", email: "bill@info.com", role: "Subscriber", userId: 0 };
 			$scope.userCounter += 1;
 			$scope.user.userId = $scope.userCounter;
 			$scope.userList.push($scope.user);
 			$scope.user = {};
 
-			
+
 		}
 	})
 	.controller("AllUsersController", function ($scope, $http) {
@@ -119,20 +130,20 @@ var userApp = angular
 					$scope.total_pages = response.data.total_pages
 				});
 		}
-	
+
 		$scope.getDataPerPage = function (page_number) {
 			$http.get("https://reqres.in/api/users?page=" + page_number)
 				.then(function (response) {
 					$scope.response_list.push(response.data);
 				});
 		}
-	
+
 		$scope.loopPages = function () {
-	
+
 			$scope.getPageCount();
-	
+
 			$scope.response_list = [];
-	
+
 			for (var i = 1; i <= $scope.total_pages; i++) {
 				$scope.getDataPerPage(i);
 			}
